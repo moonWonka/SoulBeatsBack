cedbb97643eab49822bc2d8c99f206822bf61489
Wonka <guillermo.hurtado02@inacapmail.cl>
2025-05-25 20:14:02 -0400

    Add IGoogleAuthService interface for Google authentication operations

---
+++ BackendSoulBeats.Domain/Application/Services/IGoogleAuthService.cs
@@ -0,0 +1,26 @@
+namespace BackendSoulBeats.Domain.Application.Services
+{
+    public interface IGoogleAuthService
+    {
+        /// <summary>
+        /// Interfaz que define las operaciones relacionadas con el servicio de autenticación de Google.
+        /// </summary>
+        public interface IGoogleAuthService
+        {
+            /// <summary>
+            /// Registra un nuevo usuario utilizando el servicio de Google.
+            /// </summary>
+            /// <param name="email">Correo electrónico del usuario.</param>
+            /// <param name="password">Contraseña del usuario.</param>
+            /// <returns>Un valor booleano que indica si el registro fue exitoso.</returns>
+            Task<bool> RegisterUserAsync(string email, string password);
+
+            /// <summary>
+            /// Verifica si un usuario ya está registrado en el sistema de Google.
+            /// </summary>
+            /// <param name="email">Correo electrónico del usuario.</param>
+            /// <returns>Un valor booleano que indica si el usuario ya está registrado.</returns>
+            Task<bool> IsUserRegisteredAsync(string email);
+        }
+    }
+}
